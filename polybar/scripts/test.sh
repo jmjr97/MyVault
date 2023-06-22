8d45877b99f38322c785487eb785c6ec273b3fad
John Majka <jmjr97@gmail.com>
2023-06-22 03:21:51 -0400

    alacritty theme

--- polybar/scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#pactl set-default-sink alsa_output.usb-Logitech_PRO_000000000000-00.pro-output-0
-
-SINK=$(pactl get-default-sink)
-VOLUME=$(pactl get-sink-volume $SINK $N)
-echo "$SINK $VOLUME"
