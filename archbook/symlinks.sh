b58d8acf03977b0badc065ad8b2d0c0f97e4d617
John Majka Jr. <jmjr97@gmail.com>
2024-10-23 08:59:40 -0400

    added archbook directory

---
+++ archbook/symlinks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# mkdir ~/.config
+
+# ln -s /home/john/myrepo/dunst/ /home/john/.config/
+ln -s /home/john/myrepo/archbook/hypr/ /home/john/.config/
+ln -s /home/john/myrepo/archbook/nwg-look/ /home/john/.config/
+ln -s /home/john/myrepo/archbook/waybar/ /home/john/.config/
+ln -s /home/john/myrepo/archbook/wlogout/ /home/john/.config/
+
+# ln -s /home/john/myrepo/archbook/kitty/ /home/john/.config/
+# ln -s /home/john/myrepo/archbook/nvim/ /home/john/.config/
+# ln -s /home/john/myrepo/archbook/yazi/ /home/john/.config/
+# ln -s /home/john/myrepo/archbook/zsh/ /home/john/.config/
+
+# ln -s /home/john/myrepo/archbook/starship.toml /home/john/.config/
+# ln -s /home/john/myrepo/archbook/.themes/ /home/john/
