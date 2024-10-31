01ee2f602d8cc2478681af79a96e3758b95e5bf2
John Majka <jmjr97@gmail.com>
2024-10-30 20:22:04 -0400

    polybar config

--- polybar/scripts/power-mode.sh
+++ polybar/scripts/power-mode.sh
@@ -2,18 +2,26 @@
 
 function main() {
 
-    CHECK=$(xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -v)
+  CHECK=$(xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -v)
 
-    action=$1
-    if [ "${action}" == "swap" ]; then
-        xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -T
+  action=$1
+  if [ "${action}" == "swap" ]; then
+    xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -T
+
+    if [ ${CHECK} == "false" ]; then
+      echo %{F#ed8796}%{F-}
     else
-        if [ ${CHECK} == "false" ]; then
-            echo "󰒲"
-        else
-            echo "󰒳"
-        fi
+      echo %{F#8aadf4}󰤄%{F-}
     fi
+  fi
+
+  if [ "${action}" == "" ]; then
+    if [ ${CHECK} == "false" ]; then
+      echo %{F#8aadf4}󰤄%{F-}
+    else
+      echo %{F#ed8796}%{F-}
+    fi
+  fi
 }
 
 main $@
