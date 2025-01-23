be4b18f9ec6ad713255017f16e2bc6594ff4420d
John Majka Jr. <jmjr97@gmail.com>
2025-01-23 00:56:37 -0500

    i3 and scripts

--- myscripts/bash/rofi/launcher.sh
+++ myscripts/bash/rofi/launcher.sh
@@ -3,18 +3,20 @@
 main () {
   case $@ in
     lazygit)
-      kitty --class="__lazygit" lazygit -p ~/myrepo/
-      # wezterm start --always-new-process lazygit -p ~/myrepo/
+      # kitty --class="__lazygit" lazygit -p ~/myrepo/
+      wezterm --config-file ~/.config/wezterm/wezterm-float.lua start --class="__lazygit" lazygit -p ~/myrepo/
       ;;
     btop)
       # kitty --class="__btop" btop
       wezterm --config-file ~/.config/wezterm/wezterm-float.lua start --class="__btop" btop
       ;;
     bluetui)
-      kitty --class="__bluetui" bluetui
+      # kitty --class="__bluetui" bluetui
+      wezterm --config-file ~/.config/wezterm/wezterm-float.lua start --class="__bluetui" bluetui
       ;;
     nmtui)
-      kitty --class="__nmtui" nmtui
+      # kitty --class="__nmtui" nmtui
+      wezterm --config-file ~/.config/wezterm/wezterm-float.lua start --class="__nmtui" nmtui
       ;;
   esac
 }
