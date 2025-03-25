70fb4a7e885a33a768ddb075e1c2ac98dddd93b4
John Majka Jr. <jmjr97@gmail.com>
2025-03-25 11:08:18 -0400

    3-25-25

--- dotfiles/nvim/lua/plugins/oil.lua
+++ dotfiles/nvim/lua/plugins/oil.lua
@@ -87,10 +87,10 @@ return {
       use_default_keymaps = true,
       view_options = {
         -- Show files and directories that start with "."
-        show_hidden = true,
+        show_hidden = false,
         -- This function defines what is considered a "hidden" file
         is_hidden_file = function(name, bufnr)
-          return vim.startswith(name, ".")
+          return vim.startswith(name, "..")
         end,
         -- This function defines what will never be shown, even when `show_hidden` is set
         is_always_hidden = function(name, bufnr)
