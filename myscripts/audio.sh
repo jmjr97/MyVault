785966c9e33d0f7047e1d474a692b9e536bac884
John Majka Jr <jmjr97@gmail.com>
2025-10-21 15:33:56 -0400

    10-21-25

--- myscripts/audio.sh
+++ myscripts/audio.sh
@@ -2,7 +2,9 @@
 
 # speakers='alsa_output.pci-0000_07_00.1.hdmi-stereo-extra1'
 speakers='alsa_output.pci-0000_09_00.4.analog-stereo'
-headphones='alsa_output.usb-Logitech_PRO_000000000000-00.analog-stereo'
+headphones='alsa_output.usb-Logitech_PRO_000000000000-00.pro-output-0'
+# headphones='alsa_output.usb-Logitech_PRO_000000000000-00.analog-stereo'
+
 SINK=$(pactl get-default-sink NAME)
 
 function main() {
--- myscripts/launchers/notes.sh
+++ myscripts/launchers/notes.sh
@@ -2,7 +2,7 @@
 
 # selection=$(find ~/Documents/obsidian-sync/ -name '*.md' | rofi -dmenu -i " Notes:")
 
-selection=$(fd . --type file $HOME/Documents/obsidian-sync/ 2>/dev/null | \
+selection=$(fd . --type file $HOME/Dropbox/obsidian/ 2>/dev/null | \
     sed "s;$HOME/Documents/obsidian-sync/;~;" | \
     rofi -m -1 -sort -sorting-method fzf -disable-history -dmenu -no-custom -p -i " Notes:" | \
     sed "s;\~;$HOME/Documents/obsidian-sync/;"
